6af35b76d247c326e3b6b61b2a0e2f85c0fe82ab
mariolil <loic.marioli@etu.univ-grenoble-alpes.fr>
2024-06-16 01:15:19 +0200

    fix navbar

--- main.js
+++ main.js
@@ -4,13 +4,16 @@ line.classList.add('line');
 
 nav.appendChild(line);
 
-let active = nav.querySelector('.active');
+let active = nav.querySelector('.active a');
 let posActive = 0;
 let widActive = 0;
 
 if(active) {
-  posActive = active.getBoundingClientRect().left;
-  widActive = active.offsetWidth-2;
+  let activeRect = active.getBoundingClientRect();
+  let navRect = nav.getBoundingClientRect();
+  console.log(activeRect.left, navRect.left)
+  posActive = activeRect.left - navRect.left;
+  widActive = active.offsetWidth;
   line.style.left = posActive + 'px';
   line.style.width = widActive + 'px';
   line.style.transition = 'all 0.3s ease'; 
@@ -19,43 +22,42 @@ if(active) {
 nav.querySelectorAll('nav ul li a').forEach(function(a) {
   a.addEventListener('click', function(e) {
     e.preventDefault();
-    
-      let position = a.getBoundingClientRect().left;
-      let width = a.offsetWidth;
-      
 
-      if(position >= posActive) {
-        line.style.width = ((position - posActive) + width) + 'px';
-        setTimeout(function() {
-          line.style.width = width + 'px';
-          line.style.left = position + 'px';
-          setTimeout(function() {
-            window.location.href = a.href;
-         }, 200);
-        }, 200);
-      } else {
+    let aRect = a.getBoundingClientRect();
+    let navRect = nav.getBoundingClientRect();
+    let position = aRect.left - navRect.left;
+    let width = a.offsetWidth;
+
+    if(position >= posActive) {
+      line.style.width = ((position - posActive) + width) + 'px';
+      setTimeout(function() {
+        line.style.width = width + 'px';
         line.style.left = position + 'px';
-        line.style.width = ((posActive - position) + widActive) + 'px';
         setTimeout(function() {
-          line.style.width = width + 'px';
-          setTimeout(function() {
-            window.location.href = a.href;
-          }, 200);
+          window.location.href = a.href;
         }, 200);
-      }
-
-    
+      }, 200);
+    } else {
+      line.style.left = position + 'px';
+      line.style.width = ((posActive - position) + widActive) + 'px';
+      setTimeout(function() {
+        line.style.width = width + 'px';
+        setTimeout(function() {
+          window.location.href = a.href;
+        }, 200);
+      }, 200);
+    }
   });
 });
 
-
 window.addEventListener('resize', function() {
-  line.style.transition = 'none'; 
+  line.style.transition = 'none';
   if(active) {
-    posActive = active.getBoundingClientRect().left;
+    let activeRect = active.getBoundingClientRect();
+    let navRect = nav.getBoundingClientRect();
+    posActive = activeRect.left - navRect.left;
     widActive = active.offsetWidth;
     line.style.left = posActive + 'px';
     line.style.width = widActive + 'px';
   }
 });
-
