108ece35a38abc9e4dcbe2b8d33a139ff44c03ff
happx <happxjunior@gmail.com>
2025-03-22 10:12:29 -0400

    project

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
+    navControl();
+    getSkills();
+    getProjects();
+});
+
+function navControl() {
     const sections = document.querySelectorAll('section');
     const navLinks = document.querySelectorAll('.nav-link a');
     const nav = document.getElementsByTagName('nav')[0];
@@ -27,4 +33,122 @@ document.addEventListener('DOMContentLoaded', function() {
             
         });
     });
-});
+}
+
+function getSkills(){
+    fetch('./json/skills.json')
+        .then(response => response.json())
+        .then(skills => {
+            const skillList = document.querySelector('.skill-list');
+            
+            skills.forEach(skill => {
+                const card = document.createElement('div');
+                card.classList.add('card-skill');
+        
+                const h2 = document.createElement('h2');
+                h2.textContent = skill.title;
+                card.appendChild(h2);
+        
+                const ul = document.createElement('ul');
+                skill.items.forEach(item => {
+                    const li = document.createElement('li');
+        
+                    const img = document.createElement('img');
+                    img.src = item.img;
+                    img.alt = item.text;
+                    li.appendChild(img);
+        
+                    const span = document.createElement('span');
+                    span.textContent = item.text;
+                    li.appendChild(span);
+        
+                    ul.appendChild(li);
+                });
+                card.appendChild(ul);
+        
+                if (skill.subsections) {
+                    skill.subsections.forEach(subsection => {
+                        const h3 = document.createElement('h3');
+                        h3.textContent = subsection.title;
+                        card.appendChild(h3);
+        
+                        const subUl = document.createElement('ul');
+                        subsection.items.forEach(subItem => {
+                            const subLi = document.createElement('li');
+        
+                            const subImg = document.createElement('img');
+                            subImg.src = subItem.img;
+                            subImg.alt = subItem.text;
+                            subLi.appendChild(subImg);
+        
+                            const subSpan = document.createElement('span');
+                            subSpan.textContent = subItem.text;
+                            subLi.appendChild(subSpan);
+        
+                            subUl.appendChild(subLi);
+                        });
+                        card.appendChild(subUl);
+                    });
+                }
+        
+                skillList.appendChild(card);
+            });
+        });
+   
+}
+
+
+function getProjects() {
+    fetch('./json/projects.json')
+        .then(response => response.json())
+        .then(projects => {
+            const projectList = document.querySelector('.project-list');
+
+            projects.forEach(project => {
+                // Créer la carte de projet
+                const card = document.createElement('div');
+                card.classList.add('project-card');
+
+                // Créer la section de l'image
+                const projectImage = document.createElement('div');
+                projectImage.classList.add('project-image');
+
+                const img = document.createElement('img');
+                img.src = project.img;
+                img.alt = project.title;
+                projectImage.appendChild(img);
+
+                const wave = document.createElementNS('http://www.w3.org/2000/svg', 'svg'); // Utilisez createElementNS pour les SVG
+                wave.classList.add('wave');
+                wave.setAttribute('viewBox', '0 0 500 150');
+                wave.setAttribute('preserveAspectRatio', 'none');
+
+                const path = document.createElementNS('http://www.w3.org/2000/svg', 'path'); // Utilisez createElementNS pour les éléments SVG
+                path.setAttribute('d', 'M0,100 C150,200 350,0 500,100 L500,150 L0,150 Z');
+                wave.appendChild(path);
+
+                projectImage.appendChild(wave);
+
+                projectImage.appendChild(wave);
+                card.appendChild(projectImage);
+
+                // Créer la section du contenu
+                const projectContent = document.createElement('div');
+                projectContent.classList.add('project-content');
+
+                const title = document.createElement('h1');
+                title.classList.add('title');
+                title.textContent = project.title;
+                projectContent.appendChild(title);
+
+                const description = document.createElement('p');
+                description.textContent = project.description;
+                projectContent.appendChild(description);
+
+                card.appendChild(projectContent);
+
+                // Ajouter la carte au conteneur de la liste des projets
+                projectList.appendChild(card);
+            });
+        });
+}
